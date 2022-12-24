4710e0dc3e1329181cde1d60533e5a6fac5d3706
Laurie Roy <laurieroy.dev@gmail.com>
2022-12-24 11:52:20 -0800

    Add sort by most recent date to getAllItems

--- lib/md.ts
+++ lib/md.ts
@@ -7,8 +7,13 @@ import html from "remark-html";
 import remarkGfm from "remark-gfm";
 import { Blog } from "@interfaces/Blog";
 
-const getAllItems = (fileNames: string[], get: (name: string)=> MarkdownItem) => {
-  const items = fileNames.map((name) => get(name));
+const getAllItems = (
+  fileNames: string[], 
+  get: (name: string) => MarkdownItem
+) => {
+  const items = fileNames
+    .map((name) => get(name))
+    .sort((item1, item2) => (item1.date > item2.date ? -1 : 1));
   return items;
 }
 const getDir = (path: string) => join(process.cwd(), path);
